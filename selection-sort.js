c1dda5cf19e7b3df8f3ea56961bfbfdc4685a27a
Ben Aston <105795506+benisastonished@users.noreply.github.com>
2022-11-17 18:48:36 +0000

    Create selection-sort.js

---
+++ selection-sort.js
@@ -0,0 +1,14 @@
+const sort = (arr) => {
+    for(let i = 0; i < arr.length; i++) {
+        let minIndex = i
+        for(let j = i; j < arr.length; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j
+            }
+        }
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+    return arr
+}
+
+sort(['c', 'a', 'f', 'b', '0'])
